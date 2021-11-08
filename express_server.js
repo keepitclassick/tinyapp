7c3755a339df9e94d0a80ed93b04f36f8b996f06
keepitclassick <keepitclassick@gmail.com>
2021-11-08 17:02:21 -0500

    added table of urls and link

--- express_server.js
+++ express_server.js
@@ -6,8 +6,8 @@ app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
+  "9sm5xK": "http://www.google.com",
+}
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -20,6 +20,13 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+
+  res.render("urls_index", templateVars);
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
