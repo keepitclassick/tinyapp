0f822cc7d5b21514fe9a8461f3c25f770174ef75
keepitclassick <keepitclassick@gmail.com>
2021-11-09 17:11:41 -0500

    added login endpoint and cookies/display username in header

--- express_server.js
+++ express_server.js
@@ -2,10 +2,11 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
-
+app.use(cookieParser())
 
 function generateRandomString() {
   let result = '';
@@ -36,17 +37,19 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { urls: urlDatabase,
+    username: req.cookies["username"] };
 
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars =  { username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"] };
   res.render("urls_show", templateVars);
 });
 
@@ -74,6 +77,10 @@ app.post("/urls/:id", (req, res) => {
   res.redirect("/urls/");
 });
 
+app.post("/login", (req, res) => {
+res.cookie("username", req.body.username);
+res.redirect("/urls/")
+})
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
