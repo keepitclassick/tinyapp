d5a38c48655b852bdcede3a1375e9e20d01b143e
keepitclassick <keepitclassick@gmail.com>
2021-11-11 10:12:08 -0500

    finished implementing the new urldatabase object and testing permisson features

--- express_server.js
+++ express_server.js
@@ -28,7 +28,7 @@ const findUserByEmail = function(email) {
   return null;
 };
 
-const userUrlList = function(id) {
+const urlsForUser = function(id) {
   let userUrls = {};
   for (let shortURL in urlDatabase) {
     if (urlDatabase[shortURL].userID === id) {
@@ -67,7 +67,7 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: userUrlList(req.cookies["user_id"]),
+  let templateVars = { urls: urlsForUser(req.cookies["user_id"]),
   user: users[req.cookies["user_id"]] }
   res.render("urls_index", templateVars);
 });
@@ -82,6 +82,7 @@ app.get("/urls/new", (req, res) => {
   }
 });
 
+//SHOWS THE VALUES OF THE SHORT URL AND ALLOWS USER TO EDIT
 app.get("/urls/:shortURL", (req, res) => {
   if (urlDatabase[req.params.shortURL]) {
     let templateVars = {
@@ -95,7 +96,7 @@ app.get("/urls/:shortURL", (req, res) => {
     res.status(404).send("The short URL does not exist.");
   }
 });
-
+//ROUTES SHORT URL TO LONG URL IF IT EXISTS, IF NOT IT WILL RETURN AN ERROR MESSAGE
 app.get("/u/:shortURL", (req, res) => {
   if (urlDatabase[req.params.shortURL]) {
     const longURL = urlDatabase[req.params.shortURL].longURL;
@@ -105,10 +106,11 @@ app.get("/u/:shortURL", (req, res) => {
       res.redirect(longURL);
     }
   } else {
-    res.status(404).send("The short URL you are trying to access does not exist.");
+    res.status(404).send("The short URL does not exist.");
   }
 });
 
+//ALLOWS USER TO ADD A URL
 app.post("/urls", (req, res) => {
   const newShortURL = generateRandomString();
   urlDatabase[newShortURL] = {
@@ -116,25 +118,34 @@ app.post("/urls", (req, res) => {
     userID: req.cookies["user_id"],
   };
   if (!req.cookies["user_id"]) {
-    res.status(403).send('You must log in to add a URL')
+    res.status(403).send("You must log in to add a URL")
   } else {
     res.redirect(`/urls/${newShortURL}`);
   }
 });
 
-//deletes url
+//DELETES URL
 app.post("/urls/:shortURL/delete", (req, res) =>{
-  const shortURL = req.params.shortURL;
-  delete urlDatabase[shortURL];
-  res.redirect("/urls");
+  let userATM = users[req.cookies["user_id"]];
+  // check if there is a current logged in user
+  if (userATM) { 
+    if (urlsForUser(userATM.id)[req.params.shortURL]) { //checking if url belongs to user 
+      delete urlDatabase[req.params.shortURL];
+      res.redirect("/urls");
+    } 
+  } else {
+    res.status(403).send("Access Denied.");
+  }
 });
 
+//ADD USER CREDENTIALS TO USER DATABASE
 app.get("/register", (req, res) => {
   let userATM = users[req.cookies["user_id"]];
   const signupVars = { email: req.body.email, password: req.body.password, user: userATM}
   res.render("register_form", signupVars)
 })
-  
+ 
+
 app.post("/register", (req, res) => { //gens new user ID and adds details to users object. Also creates a cookie for the user id.
   let newUserID = generateRandomString();
   const foundUser = findUserByEmail(req.body.email);
@@ -153,13 +164,19 @@ app.post("/register", (req, res) => { //gens new user ID and adds details to use
     return;
 })
 
-//edits specific url and redirects to main url page
-app.post("/urls/:id", (req, res) => {
-  shortURL = req.params.id;
-  urlDatabase[shortURL] = req.body.editURL;
-  res.redirect("/urls/");
+//EDITS URL
+app.post("/urls/:shortURL", (req, res) => {
+  const userID = req.cookies["user_id"];
+  const userUrls = urlsForUser(userID);
+  
+  if (Object.keys(userUrls).includes(req.params.shortURL)) {
+    const shortURL = req.params.shortURL;
+    urlDatabase[shortURL].longURL = req.body.editURL;
+    res.redirect('/urls');
+  } else {
+    res.status(403).send("Access Denied.");
+  }
 })
-
 
 app.post("/login", (req, res) => {
 const foundUser = findUserByEmail(req.body.email);
